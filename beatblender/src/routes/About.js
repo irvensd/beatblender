2215a4b7224dcda712e5d7c8230de1c30f2512c2
Irvens R Dupuy <131715069+rotcheeld@users.noreply.github.com>
2023-05-03 16:00:54 -0500

    New hero image

--- beatblender/src/routes/About.js
+++ beatblender/src/routes/About.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
+import Hero from '../components/Hero'
+// import AboutImg from "../components/image/aboutimg.jpg"
+
 
 const About = () => {
   return (
-    <div>About Irvens</div>
+    <>
+    <Navbar/>
+    <Hero
+    cName="hero"
+    // heroImg = {AboutImg}
+    title='Match your mood with our temp-based tunes!'
+    text="Login to get started"
+    buttonText="Login"
+    url="/login"
+    btnClass="show"
+    />
+ 
+      </>
   )
 }
 
